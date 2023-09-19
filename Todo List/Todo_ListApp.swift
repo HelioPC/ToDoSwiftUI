1eaacf3826a56ca8ce58bba4070be7ced0b06b97
Eliúde Vemba <sweeteliude@gmail.com>
2023-09-19 07:16:20 +0100

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Eliude Vemba on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
